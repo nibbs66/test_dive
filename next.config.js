2b2c5602319b6a631910663024065960ebf73e1b
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-09-16 13:26:47 +0200

    init commit

--- next.config.js
+++ next.config.js
@@ -16,5 +16,10 @@ module.exports = {
         ]
 
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 }
