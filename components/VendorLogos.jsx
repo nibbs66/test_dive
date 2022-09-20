883a5a746a8cd3e92bfbd9dcc2ac16bde646335b
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-09-20 12:21:32 +0200

    init commit

--- components/VendorLogos.jsx
+++ components/VendorLogos.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 const VendorLogos = ({images, as}) => {
     return (
-        <div className='flex items-center  justify-evenly h-12 w-[300vw] animate-[vendorScroll_45s_linear_infinite] bg-slate-200/70 truncate'>
+        <div className='flex items-center  justify-evenly h-24 w-[300vw] animate-[vendorScroll_45s_linear_infinite] bg-slate-200/70 truncate'>
             {images?.map((img, idx)=>(
                 img.pic.category === 'vendorLogo' &&
-                <div key={idx}  className='flex  bg-repeat-x'>
-                    <Image  src={img.pic.img} alt='' height={80}
-                            width={100} priority={true} objectFit='contain' as={as}
+                <div key={idx}  className='flex items-center py-2 bg-repeat-x'>
+                    <Image  src={img.pic.img} alt='' height={200}
+                            width={200} priority={true} objectFit='contain' as={as}
                             crossOrigin="anonymous"/>
                 </div>
 
