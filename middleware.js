798629b7a0167e350556d9d98ec713cc09904614
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-10-11 15:07:09 +0200

    update ClientHeader

--- middleware.js
+++ middleware.js
@@ -1,25 +1,19 @@
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server";
 
-export default withAuth({
-        callbacks: {
-            authorized: ({req, token })=>{
-                if(req.nextUrl.pathname === '/admin'){
-                    return token?.isAdmin === true || token?.isEmployee === true
-                }
-                if(req.nextUrl.pathname === '/admin/users/employee'){
-                    return token?.isAdmin === true
-                }
-                if(req.nextUrl.pathname === '/admin/users/customer'){
-                    return token?.isAdmin === true
-                }
-            },
-        },
+export default withAuth(function middleware(req){
+    if(req.nextUrl.pathname.startsWith('/admin') &&
+        (!req.nextauth.token?.isAdmin || !req.nextauth.token?.isEmployee)
+    ){
+        return NextResponse.redirect(new URL("/", req.url));
     }
-)
-
-export const config = { matcher: ["/admin", "/admin/users/employee", "/admin/users/customer"] }
-
+    if(req.nextUrl.pathname.startsWith('/admin/users') &&
+        !req.nextauth.token?.isAdmin
+    ){
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+})
+export const config = { matcher: ["/admin/:path*"] }
 /*export default withAuth({
         callbacks: {
             authorized: ({req, token })=>{
