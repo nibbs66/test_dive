78ab63aec3f84dc9dd4c06bf66820de832306a7b
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-11-21 21:54:46 +0100

    cart page updates

--- pages/service/index.js
+++ pages/service/index.js
@@ -7,37 +7,37 @@ import ClientHeader from "../../components/Client/ClientHeader";
 const Index = ({service}) => {
     const [activeIndex, setActiveIndex] = useState(1)
     return (
-        <div className={`container flex`}>
-            <div className={`flex flex-col gap-5 my-5  min-h-fit`}>
+        <div className={`flex flex-col gap-5 my-5  min-h-fit w-full px-10 `}>
             <ClientHeader title={`service`}/>
-                <div className={`px-24 my-5 h-48 md:h-full overflow-scroll text-sm md:text-base text-slate-500`}>
-                    {service.map((services)=>(
-                        <div key={services._id}>
-                            <p>{services.desc}</p>
-                        </div>
-                    ))}
-                </div>
-                {service.map((services, idx)=>(
-                    !services.desc &&
+            <div className={`px-24 my-5 h-48 md:h-full overflow-scroll text-sm md:text-sm text-slate-500`}>
+                {service.map((services)=>(
+                    <div key={services._id}>
+                        <p>{services.desc}</p>
+                    </div>
+                ))}
+            </div>
+            {service.map((services, idx)=>(
+                !services.desc &&
+                <div key={idx} className={`flex flex-col w-full`}>
                     <AccordionLayout
                         title={services.serviceType}
-                        bg={`${activeIndex === 0 ? 'bg-blue-600' : 'bg-blue-500'}`}
+                        bg={`${activeIndex === idx ? 'bg-blue-600' : 'bg-blue-500'}`}
                         text={'text-white'}
-                        mx={'mx-10'}
+                        mx={'mx-16'}
                         bodyMargin={'mx-10'}
                         index={idx}
                         activeIndex={activeIndex}
                         setActiveIndex={setActiveIndex}>
-                       <ul className={ `flex flex-col gap-4 pl-16 text-slate-500`}>
-                           {services.services.map((item, idx)=>(
-                               <li key={idx}>
-                                   {item}
-                               </li>
-                           ))}
-                       </ul>
+                        <ul className={ `flex flex-col gap-4 pl-16 text-slate-500`}>
+                            {services.services.map((item, idx)=>(
+                                <li key={idx}>
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
                     </AccordionLayout>
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
     );
 };
