507088d9de048739c12d246eff3c40b4f8d5855e
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2023-01-02 13:27:48 +0100

    cart page updates

--- components/Admin/Nieuw/Nieuw.jsx
+++ components/Admin/Nieuw/Nieuw.jsx
@@ -22,41 +22,41 @@ const Nieuw = ({showModal, setShowModal}) => {
     return (
         <>
             <ModalCard  showModal={showModal} setShowModal={setShowModal}>
-               <ul className={'flex flex-col font-bold text-slate-400  uppercase gap-4 px-10 py-5'}>
-                   <li className={'flex gap-2 cursor-pointer'}  onClick={()=>handleClick('store')}>
-                       <BanknotesIcon className={`h-5 w-5 `}/>
-                       <span >New Sale</span>
-                   </li>
+                <ul className={'flex flex-col font-bold text-slate-400  uppercase gap-4 px-10 py-5'}>
+                    <li className={'flex gap-2 cursor-pointer'}  onClick={()=>handleClick('store')}>
+                        <BanknotesIcon className={`h-5 w-5 `}/>
+                        <span >New Sale</span>
+                    </li>
 
-                   <li className={'flex gap-2 cursor-pointer'} onClick={()=>handleClick('users/user/new')}>
-                       <UserIcon className={`h-5 w-5 `}/>
-                       <span >New User</span>
-                   </li>
-                   <li className={'flex gap-2 cursor-pointer'}   onClick={()=>handleClick('products/new')}>
-                       <RectangleGroupIcon  className={`h-5 w-5 `}/>
-                       <span >New Product</span>
-                   </li>
-                   <li className={'flex gap-2 cursor-pointer'}   onClick={()=>handleClick('vendors/new')}>
-                       <BuildingStorefrontIcon  className={`h-5 w-5`}/>
-                       <span>New Vendor</span>
-                   </li>
+                    <li className={'flex gap-2 cursor-pointer'} onClick={()=>handleClick('users/user/new')}>
+                        <UserIcon className={`h-5 w-5 `}/>
+                        <span >New User</span>
+                    </li>
+                    <li className={'flex gap-2 cursor-pointer'}   onClick={()=>handleClick('products/new')}>
+                        <RectangleGroupIcon  className={`h-5 w-5 `}/>
+                        <span >New Product</span>
+                    </li>
+                    <li className={'flex gap-2 cursor-pointer'}   onClick={()=>handleClick('vendors/new')}>
+                        <BuildingStorefrontIcon  className={`h-5 w-5`}/>
+                        <span>New Vendor</span>
+                    </li>
 
-                   <li className={'flex gap-2 cursor-pointer'}>
-                       <WrenchIcon className={`h-5 w-5 `}/>
-                       <span >Book Service</span>
-                   </li>
+                    <li className={'flex gap-2 cursor-pointer'}>
+                        <WrenchIcon className={`h-5 w-5 `}/>
+                        <span >Book Service</span>
+                    </li>
 
-                   <li className={'flex gap-2 cursor-pointer'}>
-                       <ClockIcon   className={`h-5 w-5 `}/>
-                       <span >Book Rental</span>
-                   </li>
-                   <li className={'flex gap-2 cursor-pointer'}>
-                       <AcademicCapIcon  className={`h-5 w-5 `}/>
-                       <span >Book Lesson</span>
-                   </li>
+                    <li className={'flex gap-2 cursor-pointer'} onClick={()=>handleClick('rental_equipment/new')}>
+                        <ClockIcon   className={`h-5 w-5 `}/>
+                        <span >New Rental</span>
+                    </li>
+                    <li className={'flex gap-2 cursor-pointer'} onClick={()=>handleClick('cursus/new')}>
+                        <AcademicCapIcon  className={`h-5 w-5 `}/>
+                        <span >New Cursus</span>
+                    </li>
 
 
-               </ul>
+                </ul>
 
             </ModalCard>
 
