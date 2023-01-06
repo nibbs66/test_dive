e130a56c445db9c14c64e920b725f240dab1ad42
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2023-01-06 17:09:23 +0100

    cart page updates

--- components/ImageSelectors/UploadImg.jsx
+++ components/ImageSelectors/UploadImg.jsx
@@ -4,40 +4,43 @@ import {ArrowUpTrayIcon} from '@heroicons/react/24/outline'
 const UploadImg = ({setFile, setUpload, upload, handleClick, file}) => {
     return (
 
-            <div className="flex items-center  justify-center w-full  gap-10">
-                <div>
-                    <Image
-                        className={`object-cover rounded-full items-center`}
-                        src={ file.length !== 0
-                            ? URL.createObjectURL(file)
+        <div className="flex items-center  justify-center w-full gap-10">
+            <div className={`shrink-0`}>
+                <Image
+                    className={`object-cover rounded-full items-center`}
+                    src={ file.length !== 0
+                        ? URL.createObjectURL(file)
 
-                            :  "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
-                        alt={''}
-                        width={100} height={100} objectFit='cover'
-                    />
-                </div>
-                {!upload &&  <div className={`flex items-center justify-center cursor-pointer `}>
-                    <label className={`flex items-center gap-2 text-slate-400 cursor-pointer`} htmlFor="file">
-                        Choose File: <ArrowUpTrayIcon className={`h-5 w-5`}/>
-                    </label>
-                    <input
-                        type="file"
-                        id="file"
-                        onChange={(e)=> {
-                            setFile(e.target.files[0])
-                            setUpload(true)
-                        }}
-                        style={{display: "none"}}
-                    />
-                </div>}
+                        :  "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
+                    alt={''}
+                    width={100} height={100} objectFit='cover'
+                />
 
-                <div className={`flex  items-center gap-2`}>
-                    {upload && <button
-                        onClick={handleClick}
-                        className={`bg-green-500 text-white uppercase py-1 px-2 rounded text-sm font-bold`}>Add
-                        Img</button>}
-                </div>
             </div>
+            {!upload &&  <div className={`flex items-center justify-center cursor-pointer `}>
+
+                <input
+                    className={`flex file:bg-[#3b81f6] file:text-white file:mr-4 file:py-2 text-sm text-slate-400
+                                    file:uppercase file:text-sm file:px-4 file:rounded-full file:border-0
+                                    file:cursor-pointer
+                                    `}
+                    type="file"
+                    id="file"
+                    onChange={(e)=> {
+                        setFile(e.target.files[0])
+                        setUpload(true)
+                    }}
+
+                />
+            </div>}
+
+            <div className={`flex  items-center gap-2`}>
+                {upload && <button
+                    onClick={handleClick}
+                    className={`bg-green-500 text-white uppercase py-1 px-2 rounded text-sm font-bold`}>Add
+                    Img</button>}
+            </div>
+        </div>
     );
 };
 
